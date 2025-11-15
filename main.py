35d3d75b91f3b27fa40f6aff5df194b948443aad
leemonsharc <146793021+leemonsharc@users.noreply.github.com>
2025-11-14 21:05:27 -0800

    Update main.py
    Pass termcont variable to the index template.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #main.py
 from flask import Flask, render_template
 app = Flask(__name__)
+termcont = "> "
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', content = termcont)
 if __name__ == '__main__':
     app.run()
