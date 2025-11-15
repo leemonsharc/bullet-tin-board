4f2344857bfafef64681b9a826154adcbf4cfb92
leemonsharc <146793021+leemonsharc@users.noreply.github.com>
2025-11-15 05:14:28 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+#main.py
 from flask import Flask, request, render_template
+
+def processCommand(command):
+    output = ""
+    cmdsplits = command.split()
+    if command == "/?" or command == "help":
+        output = """
+echo [text]-- writes [text] to the terminal
+        """
+    elif cmdsplits[0] == "echo":
+        if cmdsplits[1]:
+            output = cmdsplits[1]
+    else:
+        output = "Invalid command. Type 'help' or '/?' for help."
+    return output
+
 app = Flask(__name__)
 history = []
 @app.route('/')
@@ -6,16 +22,13 @@ def index():
     global history
     print(history)
     return render_template('index.html', history = history)
-@app.route('/submit', methods=['POST'])
+@app.route('/', methods=['POST'])
 def cmdhistory():
     global history
-    joinedword = ">" + "".join(request.form['inp'])
-    history.append(joinedword)
-    return """
-    <!DOCTYPE html>
-    <head>
-        <meta http-equiv="refresh" content="0; URL=/" />
-    </head>
-    """
+    command = "".join(request.form['inp'])
+    processedCommand = processCommand(command)
+    history.append("> " + command)
+    history.append(processedCommand)
+    return render_template('index.html', history = history)
 if __name__ == '__main__':
     app.run()
